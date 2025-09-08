56334555e21ffa9efd874377c5d40d69dee7ae13
Flávio Rocha Lamberti <156143187+Flanisk@users.noreply.github.com>
2025-09-07 22:46:20 -0300

    Add files via upload

---
+++ EX12_ExibeNome.java
@@ -0,0 +1,8 @@
+public class EX12_ExibeNome {
+    public static void main(String[] args) {
+        String nome;
+        nome = "Flavin du pineu";
+
+        System.out.println(nome);
+    }
+}
---
+++ EX12_ExibeValores.java
@@ -0,0 +1,12 @@
+public class EX12_ExibeValores {
+    public static void main(String[] args){
+        byte bite;
+        bite = 19;
+
+
+        short calc;
+        calc = 1571;
+
+        System.out.println(bite + " " + calc);
+    }
+}
