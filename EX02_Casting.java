ad9ccc63e0e51624422c88bb42f8616b9f81d97b
Flávio Rocha Lamberti <156143187+Flanisk@users.noreply.github.com>
2025-09-23 09:39:23 -0300

    Add files via upload

---
+++ EX02_Casting.java
@@ -0,0 +1,9 @@
+public class EX02_Casting {
+    public static void main(String[] args){
+        long numero1 =  2147483648L;
+
+        int numero2 = (int) 2147483648L;
+
+        System.out.println("numero1: " + numero1 + "" + "         numero2: " + numero2);
+    }
+}
---
+++ EX07_OverflowUnderflow.java
@@ -0,0 +1,7 @@
+public class EX07_OverflowUnderflow {
+        public static void main(String[] args) {
+            System.out.println(2147483647 + 1);
+            System.out.println(-2147483647 - 1);
+        }
+
+}
